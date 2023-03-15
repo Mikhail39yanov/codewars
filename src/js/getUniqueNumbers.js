26a43af1b52665d76dbc93f7a924f5a82a8efdae
Mikhail Yanov <yanovmikhailco@ya.ru>
2023-03-15 09:42:44 +0200

    update getUniqueNumbers

--- src/js/getUniqueNumbers.js
+++ src/js/getUniqueNumbers.js
@@ -8,7 +8,9 @@ function getUniqueNumbers(inputArray) {
   for (let i = 0; i < inputArray.length; i++) {
     const number = inputArray[i]
 
-    if (!uniqueArr.includes(number)) {
+    const isCheck = checkHash(number, uniqueArr)
+
+    if (isCheck) {
       uniqueArr[k] = number
       k++
     }
@@ -16,6 +18,41 @@ function getUniqueNumbers(inputArray) {
 
   console.log(uniqueArr)
   return uniqueArr
+}
+
+function hashInt(input) {
+  let int = 1
+  let remainderDivision
+
+  while (input !== 0) {
+    remainderDivision = input % 10
+
+    if (remainderDivision !== 0) {
+      int *= remainderDivision
+    }
+
+    input = Math.trunc(input / 10)
+  }
+
+  return int
+}
+
+function checkHash(input, arr) {
+  const hash = hashInt(input)
+
+  if (arr.length === 0) {
+    return true
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    const hashUnique = hashInt(arr[i])
+
+    if (hash === hashUnique) {
+      return false
+    }
+  }
+
+  return true
 }
 
 getUniqueNumbers(numbers)
