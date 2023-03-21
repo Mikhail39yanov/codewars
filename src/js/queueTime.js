90290c31b6e547912441ec4ae6d9593aa562077f
Mikhail Yanov <yanovmikhailco@ya.ru>
2023-03-21 16:24:34 +0200

    add queueTime

---
+++ src/js/queueTime.js
@@ -0,0 +1,21 @@
+// "exec": "npx node ./src/js/index.js"
+// import { decodeMorse } from './decodeMorse.js'
+function queueTime(customers, n) {
+  const registers = new Array(n).fill(0)
+
+  for (let waitTime of customers) {
+    // находим кассу с минимальным временем ожидания
+    const minWaitTime = registers.indexOf(Math.min(...registers))
+
+    // добавляем время клиента к этой кассе
+    registers[minWaitTime] += waitTime
+  }
+
+  return Math.max(...registers)
+}
+
+// queueTime([], 1) // 0
+// queueTime([1, 2, 3, 4], 1) //10
+// queueTime([2, 2, 3, 3, 4, 4], 2) // 9
+// queueTime([1, 2, 3, 4, 5], 100) // 5
+// queueTime([2, 3, 10, 2], 2) // 12
